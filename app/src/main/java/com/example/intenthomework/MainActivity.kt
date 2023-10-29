66aac3db6fd096dbb97231d2216cd9781b5c9ffe
faizalrizqikholily <faizalrizqikholily26@gmail.com>
2023-10-29 12:29:04 +0700

    update

---
+++ app/src/main/java/com/example/intenthomework/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.intenthomework
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.*
+import com.example.intenthomework.ui.theme.IntentHomeworkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntentHomeworkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Greeting() {
+    val context = LocalContext.current
+    val intent = Intent(context, SecondActivity::class.java)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Concert Banner",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = "Concert Name",
+            modifier = Modifier.padding(16.dp),
+            style = TextStyle(fontSize = 18.sp)
+        )
+        Button(
+            onClick = {
+                startActivity(context, intent, null)
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Detail")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IntentHomeworkTheme {
+        Greeting()
+    }
+}
---
+++ app/src/main/java/com/example/intenthomework/SecondActivity.kt
@@ -0,0 +1,84 @@
+package com.example.intenthomework
+
+import android.content.Intent
+import android.os.Bundle
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.*
+import com.example.intenthomework.ui.theme.IntentHomeworkTheme
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntentHomeworkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SecondPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondPage() {
+    val context = LocalContext.current
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/jKk7ub4w6UdksDcX9n"))
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Concert Banner",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = "Concert Detail",
+            modifier = Modifier.padding(16.dp),
+            style = TextStyle(fontSize = 18.sp)
+        )
+        Button(
+            onClick = {
+                startActivity(context, intent, null)
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Maps")
+        }
+        //Button(
+        // onClick = {
+        // Handle buy ticket logic here
+        // },
+        // modifier = Modifier
+        //    .padding(16.dp)
+        //    .align(Alignment.End)
+        // ) {
+        //  Text(text = "Buy Ticket")
+        //  }
+    }
+}
